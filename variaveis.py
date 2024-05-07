c28b70f262882ccb01feaa17910289d88e873fe8
Naat08 <ribeirodesouzanatalia16@gmail.com>
2024-05-07 15:14:12 -0300

    Create variaveis.py

---
+++ variaveis.py
@@ -0,0 +1,12 @@
+nome="Natália Ribeiro de Souza"
+idade=15
+email="ribeirodesouzanatalia16@gamil.com"
+telefone='17 99123-4567'
+nota=8.99
+aprovado=True
+print(nome)
+print(idade)
+print(email)
+print(telefone)
+print(nota)
+print(aprovado)
